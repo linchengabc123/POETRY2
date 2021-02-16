27a9cfe96ac1d105bc1ae414ab982f3f30d02e52
linchengabc123 <78034936+linchengabc123@users.noreply.github.com>
2021-02-16 16:45:05 +0800

    Update ranking list.js

--- mainProject-new/miniprogram/pages/ranking list/ranking list.js
+++ mainProject-new/miniprogram/pages/ranking list/ranking list.js
@@ -1,18 +1,125 @@
-// miniprogram/pages/ranking list/ranking list.js
+// pages/demo/demo.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    dataArray: [],
+    wrongNum: 0,
+    curIndex: 0,
+    flag: false
+  },
 
+  check1() {
+    var idx = this.data.curIndex
+    console.log(this.data.dataArray[idx].answer)
+    if (this.data.dataArray[idx].answer == 'A') {
+      if (this.data.curIndex == 19) {
+        this.setData({
+          flag: true
+        })
+      }
+      wx.showToast({
+        title: '答案正确',
+        icon: 'success',
+        duration: 1500//持续的时间
+      })
+      this.setData({
+        curIndex: idx + 1
+      })
+    }
+    else {
+      wx.showToast({
+        title: '选择错误，请重新选择',
+        icon: 'none',
+        duration: 2000//持续的时间
+      })
+      this.setData({
+        wrongNum: this.data.wrongNum+1
+      })
+    }
+  },
+  check2() {
+    
+    var idx = this.data.curIndex
+    console.log(this.data.dataArray[idx].answer)
+    if (this.data.dataArray[idx].answer == 'B') {
+      if (this.data.curIndex == 19) {
+        this.setData({
+          flag: true
+        })
+      }
+      wx.showToast({
+        title: '答案正确',
+        icon: 'success',
+        duration: 1500//持续的时间
+      })
+      this.setData({
+        curIndex: idx + 1
+      })
+    }
+    else {
+      wx.showToast({
+        title: '选择错误，请重新选择',
+        icon: 'none',
+        duration: 2000//持续的时间
+      })
+      this.setData({
+        wrongNum: this.data.wrongNum+1
+      })
+      
+    }
+  },
+  check3() {
+    
+    var idx = this.data.curIndex
+    console.log(this.data.dataArray[idx].answer)
+    if (this.data.dataArray[idx].answer == 'C') {
+      if (this.data.curIndex == 19) {
+        this.setData({
+          flag: true
+        })
+      }
+      wx.showToast({
+        title: '答案正确',
+        icon: 'success',
+        duration: 1500//持续的时间
+      })
+      this.setData({
+        curIndex: idx + 1
+      })
+    }
+    else {
+      wx.showToast({
+        title: '选择错误，请重新选择',
+        icon: 'none',
+        duration: 2000//持续的时间
+      })
+      this.setData({
+        wrongNum: this.data.wrongNum+1
+      })
+    }
+  },
+
+  test() {
+    this.setData({
+      curIndex: this.data.curIndex + 1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.cloud.callFunction({
+      name:'getData2'
+    }).then(res=>{
+      console.log(res.result.data)
+      this.setData({
+          dataArray:res.result.data
+      })
+    })
   },
 
   /**
